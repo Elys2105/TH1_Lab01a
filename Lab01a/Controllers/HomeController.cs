b6234810d58bce1a69bfa3d4bce8a643632b31d8
chaun <chaun@MSI>
2024-09-17 00:54:51 +0700

    Add project files.

---
+++ Lab01a/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01a.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Bai1()
+        {
+            ViewBag.Message = "Tóm tắt bài báo";
+            return View();
+        }
+        public ActionResult Bai2()
+        {
+            ViewBag.Message = "Hiển thị công thức";
+
+            return View();
+        }
+
+        public ActionResult Bai3()
+        {
+            ViewBag.Message = "Tạo bảng";
+
+            return View();
+        }
+    }
+}
